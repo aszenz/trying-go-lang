fcd2518bc6d6fac273e75301894efd5d03758936
asrar <aszenz@gmail.com>
2019-07-27 23:22:03 +0530

    Added SlicesSum + tests

--- go-arrays/arrays.go
+++ go-arrays/arrays.go
@@ -1,9 +1,19 @@
 package arrays
 
+// ArraySum sums up the values of array
 func ArraySum(numbers []int) int {
 	sum := 0
 	for _, value := range(numbers) {
 		sum += value
 	}
 	return sum
+}
+
+// SlicesSum sums up multiple slices and returns an array with their sum
+func SlicesSum(slices [][]int) []int {
+	var sums []int
+	for _, arr := range(slices) {
+		sums = append(sums, ArraySum(arr))
+	}
+	return sums
 }
--- go-arrays/arrays_test.go
+++ go-arrays/arrays_test.go
@@ -2,15 +2,9 @@ package arrays
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
-
-func ExampleArraySum() {
-	numbers := []int{1, 2, 3, 4, 5}
-	res := ArraySum(numbers)
-	fmt.Println(res)
-	// Output:15
-}
 
 func TestArraySum(t *testing.T) {
 	t.Run("collection of any size of numbers", func(t *testing.T) {
@@ -23,9 +17,41 @@ func TestArraySum(t *testing.T) {
 	})
 }
 
+func ExampleArraySum() {
+	numbers := []int{1, 2, 3, 4, 5}
+	res := ArraySum(numbers)
+	fmt.Println(res)
+	// Output:15
+}
+
 func BenchmarkArraySum(b *testing.B) {
 	numbers := []int{1, 2, 3, 4, 5}
 	for i := 0; i < b.N; i++ {
 		ArraySum(numbers)
+	}
+}
+
+func TestSlicesSum(t *testing.T) {
+	t.Run("sum slices", func(t *testing.T) {
+		slices := [][]int{[]int{1, 2}, []int{3, 4}, []int{5}}
+		got := SlicesSum(slices)
+		expected := []int{3, 7, 5}
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("expected %d but got %d given %v", expected, got, slices)
+		}
+	})
+}
+
+func ExampleSlicesSum() {
+	slices := [][]int{[]int{1, 2}, []int{3, 4}, []int{9, 2}}
+	res := SlicesSum(slices)
+	fmt.Println(res)
+	// Output:[3 7 11]
+}
+
+func BenchmarkSlicesSum(b *testing.B) {
+	slices := [][]int{[]int{1, 2}, []int{3, 4}, []int{9, 2}}
+	for i := 0; i < b.N; i++ {
+		SlicesSum(slices)
 	}
 }
