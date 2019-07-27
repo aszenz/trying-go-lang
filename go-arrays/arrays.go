1a3a9bf2961e647c48ed5da36fac67f97816c221
asrar <aszenz@gmail.com>
2019-07-28 00:30:00 +0530

    Adds slices tail sum

--- go-arrays/arrays.go
+++ go-arrays/arrays.go
@@ -3,7 +3,7 @@ package arrays
 // ArraySum sums up the values of array
 func ArraySum(numbers []int) int {
 	sum := 0
-	for _, value := range(numbers) {
+	for _, value := range numbers {
 		sum += value
 	}
 	return sum
@@ -12,8 +12,20 @@ func ArraySum(numbers []int) int {
 // SlicesSum sums up multiple slices and returns an array with their sum
 func SlicesSum(slices [][]int) []int {
 	var sums []int
-	for _, arr := range(slices) {
+	for _, arr := range slices {
 		sums = append(sums, ArraySum(arr))
 	}
 	return sums
+}
+
+// SlicesTailSum sums up multiple slices except their first member and returns an array with their sum
+func SlicesTailSum(slices [][]int) (sums []int) {
+	for _, arr := range slices {
+		if len(arr) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, ArraySum(arr[1:]))
+		}
+	}
+	return
 }
--- go-arrays/arrays_test.go
+++ go-arrays/arrays_test.go
@@ -6,6 +6,13 @@ import (
 	"testing"
 )
 
+func checkSlicesSum(t *testing.T, got []int, want []int, given [][]int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("expected %d but got %d given %v", want, got, given)
+	}
+}
+
 func TestArraySum(t *testing.T) {
 	t.Run("collection of any size of numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
@@ -36,9 +43,7 @@ func TestSlicesSum(t *testing.T) {
 		slices := [][]int{[]int{1, 2}, []int{3, 4}, []int{5}}
 		got := SlicesSum(slices)
 		expected := []int{3, 7, 5}
-		if !reflect.DeepEqual(got, expected) {
-			t.Errorf("expected %d but got %d given %v", expected, got, slices)
-		}
+		checkSlicesSum(t, got, expected, slices)
 	})
 }
 
@@ -53,5 +58,34 @@ func BenchmarkSlicesSum(b *testing.B) {
 	slices := [][]int{[]int{1, 2}, []int{3, 4}, []int{9, 2}}
 	for i := 0; i < b.N; i++ {
 		SlicesSum(slices)
+	}
+}
+
+func TestSlicesTailSum(t *testing.T) {
+	t.Run("sum slices", func(t *testing.T) {
+		slices := [][]int{[]int{1, 2, 3}, []int{3, 4}, []int{5}, []int{2, 3, 6, 5}}
+		got := SlicesTailSum(slices)
+		expected := []int{5, 4, 0, 14}
+		checkSlicesSum(t, got, expected, slices)
+	})
+	t.Run("safely run sum slices", func(t *testing.T) {
+		slices := [][]int{[]int{2, 3, 4}, []int{}, []int{2, 9, 1}}
+		got := SlicesTailSum(slices)
+		expected := []int{7, 0, 10}
+		checkSlicesSum(t, got, expected, slices)
+	})
+}
+
+func ExampleSlicesTailSum() {
+	slices2 := [][]int{[]int{1, 2, 3}, []int{}, []int{5}, []int{2, 3, 6, 5}}
+	res2 := SlicesTailSum(slices2)
+	fmt.Println(res2)
+	// Output:[5 0 0 14]
+}
+
+func BenchmarkSlicesTailSum(b *testing.B) {
+	slices := [][]int{[]int{1, 2, 3}, []int{3, 4}, []int{5}, []int{2, 3, 6, 5}}
+	for i := 0; i < b.N; i++ {
+		SlicesTailSum(slices)
 	}
 }
