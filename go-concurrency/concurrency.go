e89e5df90147f3c34b4573ea6c6eeee4317e9c49
asrar <aszenz@gmail.com>
2019-11-17 10:14:15 +0000

    add website racer

--- go-concurrency/concurrency.go
+++ go-concurrency/concurrency.go
@@ -1,6 +1,11 @@
 package concurrency
 
-// WebsiteChecker is a function which accepts  
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+// WebsiteChecker checks if a website is available  
 type WebsiteChecker func(string) bool
 
 type result struct {
@@ -23,4 +28,42 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 		results[result.string] = result.bool
 	}
 	return results
+}
+
+func measureHTTPResponseTime(url string) time.Duration {
+	startA := time.Now()
+	http.Get(url)
+	return time.Since(startA)
+}
+
+func inefficientWebsiteRacer(url1 string, url2 string) string {
+	url1Duration := measureHTTPResponseTime(url1)
+	url2Duration := measureHTTPResponseTime(url2)
+
+	if url1Duration < url2Duration {
+		return url1
+	}
+
+	return url2
+}
+
+func ping(url string) chan bool{
+	ch := make(chan bool)
+	go func(){
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
+}
+
+// WebsiteRacer returns the faster of the two urls
+func WebsiteRacer(url1 string, url2 string, timeout time.Duration) (string, error) {
+	select {
+	case <- ping(url1):
+		return url1, nil
+	case <- ping(url2):
+		return url2, nil
+	case <- time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", url1, url2)
+	}
 }
--- go-concurrency/concurrency_test.go
+++ go-concurrency/concurrency_test.go
@@ -4,8 +4,12 @@ import (
 	"reflect"
 	"testing"
 	"time"
+	"github.com/aszenz/basic-golang/go-test-helpers"
+	"net/http/httptest"
+	"net/http"
 )
 
+// ---------- //
 func slowStubWebsiteChecker(_ string) bool{
 	time.Sleep(20 * time.Millisecond)
 	return true
@@ -46,4 +50,40 @@ func BenchmarkCheckWebsites(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
+}
+
+func makeDelayedHTTPServer(delay time.Duration) *httptest.Server {
+	 return httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+// ---------- //
+func TestWebsiteRacer(t *testing.T) {
+	t.Run("Returns the faster of the two urls", func(t *testing.T) {
+		slowServer := makeDelayedHTTPServer(20 * time.Millisecond)	
+		fastServer := makeDelayedHTTPServer(10 * time.Millisecond)	
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		got, err := WebsiteRacer(slowServer.URL, fastServer.URL, 50*time.Millisecond)
+		want := fastServer.URL
+
+		testhelpers.AssertNoError(t, err)
+		testhelpers.AssertStringsEqual(t, got, want)
+	})
+	t.Run("Return an error if server does not respond within 10 secs", func(t *testing.T){
+		serverA := makeDelayedHTTPServer(11 * time.Millisecond)
+    serverB := makeDelayedHTTPServer(12 * time.Millisecond)
+
+    defer serverA.Close()
+    defer serverB.Close()
+
+		_, err := WebsiteRacer(serverA.URL, serverB.URL, 10*time.Millisecond)
+
+		if err == nil {
+        t.Error("expected an error but didn't get one")
+    }
+	})
 }
